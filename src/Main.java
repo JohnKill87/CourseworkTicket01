9367b2d6fde56ef04fdd3f81537ce9efd7853edb
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-05-20 16:35:54 +0300

    Решение Задачи билета 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    public static void reverseCount(int[] name) {
+        for (int i = name.length - 1; i >= 0; i--) {
+            System.out.print(name[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] apples = {5, 10, 15, 20};
+        reverseCount(apples);
     }
 }
